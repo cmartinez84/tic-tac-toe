04b1b7a3f92815b47dea8a504bc4c6cf178040cf
Chris Martinez and Larry Tunseth <chris@>
2016-08-25 11:59:20 -0700

    add un refinened function to assess whether or not a person wins on each play. currently reads success at every play

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,22 @@ var board = {
   tiles: [],
   play: function(index, input){
     this.tiles[index].letter = input;
-  }
+    if(((this.tiles[0].letter===this.tiles[1].letter) && (this.tiles[1].letter === this.tiles[2].letter)) ||
+    ((this.tiles[3].letter===this.tiles[4].letter) && (this.tiles[4].letter === this.tiles[5].letter)) ||
+    ((this.tiles[6].letter===this.tiles[7].letter) && (this.tiles[7].letter === this.tiles[8].letter)) ||
+    ((this.tiles[0].letter===this.tiles[3].letter) && (this.tiles[3].letter === this.tiles[6].letter)) ||
+    ((this.tiles[1].letter===this.tiles[4].letter) && (this.tiles[4].letter === this.tiles[7].letter)) ||
+    ((this.tiles[2].letter===this.tiles[5].letter) && (this.tiles[5].letter === this.tiles[8].letter)) ||
+    ((this.tiles[0].letter===this.tiles[4].letter) && (this.tiles[4].letter === this.tiles[8].letter)) ||
+    ((this.tiles[2].letter===this.tiles[4].letter) && (this.tiles[4].letter === this.tiles[6].letter)))
+
+      {
+      alert("I wanna know");
+      }
+    }
 }
+
+
 
 function Player(playerName, letter){
   this.playerName = playerName;
